e90d4c576487a3aef598dafea85c0029c885f253
GpMendez <prueba@gmail.com>
2024-09-12 16:02:54 -0600

    se agregaron comentarios a las funciones

--- hal/gpio/gpio.c
+++ hal/gpio/gpio.c
@@ -2,6 +2,17 @@
 
 #ifdef ESP32
 //GPIOS
+
+/************************************************************************** 
+* Function: hal_gpio_mode 
+* Preconditions: Ninguna
+* Overview: Configura el GPIO especificado como entrada o salida. 
+             Primero se restablece el pin antes de asignarle la dirección.
+* Input: gpio (número de pin), mode (INPUT o OUTPUT)
+* Output: Ninguno
+*****************************************************************************/
+
+
 void hal_gpio_mode(int gpio, int mode){
     if(mode == OUTPUT){
         gpio_reset_pin(gpio);
@@ -12,13 +23,39 @@ void hal_gpio_mode(int gpio, int mode){
         gpio_set_direction(gpio,GPIO_MODE_INPUT);
     }
 }
+
+/**************************************************************************
+* Function: hal_led_set_level
+* Preconditions: El pin debe haberse configurado previamente como salida.
+* Overview: Establece el nivel lógico (alto o bajo) de un pin GPIO.
+* Input: gpio (número de pin), state (1 = alto, 0 = bajo)
+* Output: Ninguno
+*****************************************************************************/
+
 void hal_led_set_level(int gpio,bool state){
     gpio_set_level(gpio,state);
 }
+
+/**************************************************************************
+* Function: hal_btn_read
+* Preconditions: El pin debe haberse configurado previamente como entrada.
+* Overview: Lee el valor lógico del pin GPIO especificado.
+* Input: gpio (número de pin)
+* Output: Valor leído en el GPIO (1 = alto, 0 = bajo)
+*****************************************************************************/
+
+
 bool hal_btn_read(int gpio){
 
     return gpio_get_level(gpio);
 }
+/**************************************************************************
+* Function: hal_gpio_set_pullup
+* Preconditions: El pin debe haberse configurado previamente como entrada.
+* Overview: Configura el GPIO con pull-up
+* Input: gpio (número de GPIO)
+* Output: Ninguno
+*****************************************************************************/
 void hal_gpio_set_pullup(int gpio){
      gpio_set_pull_mode(gpio,GPIO_PULLUP_ONLY);
 }
@@ -26,6 +63,14 @@ void hal_gpio_set_pullup(int gpio){
 #endif
 //PIC 18F4550
 #ifdef PIC18
+/************************************************************************** 
+* Function: hal_gpio_mode 
+* Preconditions: Ninguna
+* Overview: Configura el GPIO especificado como entrada o salida. 
+* Input: gpio (número de pin), mode (INPUT o OUTPUT)
+* Output: Ninguno
+*****************************************************************************/
+
 void hal_gpio_mode(int pin, int mode){
 
    if(pin>=D0 && pin<=D7){
@@ -40,6 +85,14 @@ void hal_gpio_mode(int pin, int mode){
    
    
 }
+/**************************************************************************
+* Function: hal_led_set_level
+* Preconditions: El pin debe haberse configurado previamente como salida.
+* Overview: Establece el nivel lógico (alto o bajo) de un pin GPIO.
+* Input: gpio (número de pin), state (1 = alto, 0 = bajo)
+* Output: Ninguno
+*****************************************************************************/
+
    
 void hal_led_set_level(int puerto,int state){
    if(state==1)
@@ -47,6 +100,15 @@ void hal_led_set_level(int puerto,int state){
    else if(state==0)
       output_low(puerto);
 }
+
+/**************************************************************************
+* Function: hal_btn_read
+* Preconditions: El pin debe haberse configurado previamente como entrada.
+* Overview: Lee el valor lógico del pin GPIO especificado.
+* Input: gpio (número de pin)
+* Output: Valor leído en el GPIO (1 = alto, 0 = bajo)
+*****************************************************************************/
+
 
 int hal_btn_read(int btn){
  if(btn==PIN_B0){
